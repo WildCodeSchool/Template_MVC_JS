cd598bbd827ed36c4d23b43240e09eb27b3e0b11
Loris <45939615+lchastanet@users.noreply.github.com>
2022-09-20 11:48:39 +0200

    add tailwind (CDN) and layouts

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
+const layouts = require("express-ejs-layouts");
 const router = require("./router");
 
 const app = express();
 app.set("views", path.join(__dirname, "../src/views"));
 app.set("view engine", "ejs");
+app.use(layouts);
+app.set("layout", "./layouts/main.ejs");
 app.use(express.json());
 
 // API routes
--- src/router.js
+++ src/router.js
@@ -5,7 +5,9 @@ const router = express.Router();
 const itemControllers = require("./controllers/itemControllers");
 
 router.get("/", (req, res) => {
-  res.render("pages/index");
+  res.render("pages/index", {
+    title: "Home",
+  });
 });
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
