4672940f506d6772f3ad1c9bcd198aa258fb7653
Julien ichard WCS <julien.y.richard@gmail.com>
2022-10-06 14:57:12 +0200

    mise en place de la route contact avec page vide

--- src/router.js
+++ src/router.js
@@ -7,5 +7,12 @@ const categoryControllers = require("./controllers/categoryControllers");
 
 router.get("/", categoryControllers.browse);
 router.get("/products", productControllers.browse);
+router.get("/contact", (req, res) => {
+  console.log(req.body);
+  res.render("pages/contact", {
+    title: "Thanks for the message",
+    form: req.body,
+  });
+});
 
 module.exports = router;
